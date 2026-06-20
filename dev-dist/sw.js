9714231d352970a651303f2b35b7415cde7b1052
Robert-Mithhran-N <darktechtamil@gmail.com>
2026-06-20 09:13:14 +0530

    feat: implement service worker caching strategy and add versioning metadata

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -86,7 +86,7 @@ define(['./workbox-b79e8dca'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.d8eo5f3og8k"
+    "revision": "0.ogcejretjns"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
